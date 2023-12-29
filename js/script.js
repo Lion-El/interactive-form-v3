44fafc80b154878a349e2909e83da5cc51584cf4
Lion-El <68742917+Lion-El@users.noreply.github.com>
2023-12-29 22:06:10 +0000

    js file added. Default js parameters set. Event listeners and behaviour for job selection, design theme and color, and activities  added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
-console.log('hello!');
+const nameInput = document.getElementById('name');
+const jobRoleInput = document.getElementById('other-job-role');
+const jobRoleSelection = document.getElementById('title');
+const colorSelection = document.getElementById('color');
+const designTheme = document.getElementById('design');
+const activities = document.getElementById('activities');
+let totalCost = 0;
+
+
+//default styles/values
+nameInput.focus();
+jobRoleInput.style.display = 'none';
+colorSelection.setAttribute('disabled', '');
+
+//display/hide text input
+jobRoleSelection.addEventListener('change', (e) => {
+    let selection = e.target.value;
+    if (selection === 'other') {
+        jobRoleInput.style.display = 'inline-block';
+    } else {
+        jobRoleInput.style.display = 'none';
+    }
+});
+
+// enable/set color option
+designTheme.addEventListener('change', () => {
+    colorSelection.removeAttribute('disabled');
+    const colorOption = document.querySelectorAll('option[data-theme]');
+    colorOption.forEach(colorTheme => {
+        designTheme.value !== colorTheme.getAttribute('data-theme') 
+        ? colorTheme.hidden = true : colorTheme.hidden = false;
+    });
+    if (designTheme.value === 'js puns') {
+        colorSelection.querySelector('[value="tomato"]').selected = false;
+        colorSelection.querySelector('[value="cornflowerblue"]').selected = true;
+    } else {
+        colorSelection.querySelector('[value="cornflowerblue"]').selected = false;
+        colorSelection.querySelector('[value="tomato"]').selected = true;
+    }
+});
+
+// display total activites cost
+activities.addEventListener('change', (e) => {
+    const costDisplay = document.getElementById('activities-cost');
+    const dataCost = e.target.getAttribute('data-cost');
+    if (e.target.checked) {
+        totalCost += parseInt(dataCost);
+        costDisplay.textContent = `Total: $${totalCost}`;
+    } else {
+        totalCost -= parseInt(dataCost);
+        costDisplay.textContent = `Total: $${totalCost}`;
+    }
+});
