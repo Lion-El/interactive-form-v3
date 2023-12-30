5f904d37cc0b6276cb98963a9eccb0f3b742d79a
Lion-El <68742917+Lion-El@users.noreply.github.com>
2023-12-30 01:12:32 +0000

    payment event listener added

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@ const jobRoleSelection = document.getElementById('title');
 const colorSelection = document.getElementById('color');
 const designTheme = document.getElementById('design');
 const activities = document.getElementById('activities');
+const paymentOptions = document.getElementById('payment');
+const payMethod = paymentOptions.getElementsByTagName('option');
+const creditCard = document.querySelector('.credit-card-box');
+const paypalSection = document.getElementById('paypal');
+const bitcoinSection = document.getElementById('bitcoin');
+const form = document.querySelector('form');
 let totalCost = 0;
 
 
 //default styles/values
-nameInput.focus();
-jobRoleInput.style.display = 'none';
-colorSelection.setAttribute('disabled', '');
+window.addEventListener('load', () => {
+    nameInput.focus();
+    jobRoleInput.style.display = 'none';
+    colorSelection.setAttribute('disabled', '');
+    paymentOptions.querySelector('[value="credit-card"]').selected = true;
+    paypalSection.style.display = 'none';
+    bitcoinSection.style.display = 'none';
+})
+
 
 //display/hide text input
 jobRoleSelection.addEventListener('change', (e) => {
@@ -50,4 +62,34 @@ activities.addEventListener('change', (e) => {
         totalCost -= parseInt(dataCost);
         costDisplay.textContent = `Total: $${totalCost}`;
     }
+});
+
+paymentOptions.addEventListener('change', () => {
+    for (let i=1; i < payMethod.length; i++ ) {
+        if (payMethod[i].value === paymentOptions.value) {
+            switch (payMethod[i].value) {
+                case 'credit-card':
+                    paypalSection.style.display = 'none';
+                    bitcoinSection.style.display = 'none';
+                    creditCard.style.display = 'flex';
+                    break;
+                case 'paypal':
+                    creditCard.style.display = 'none';
+                    bitcoinSection.style.display = 'none';
+                    paypalSection.style.display = 'block';
+                    break;
+                case 'bitcoin':
+                    creditCard.style.display = 'none';
+                    paypalSection.style.display = 'none';
+                    bitcoinSection.style.display = 'block';
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+});
+
+form.addEventListener('submit', (e) => {
+    
 });
